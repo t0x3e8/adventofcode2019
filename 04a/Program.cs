eda1ce853495c54ebe8283315aff5c2448715a9c
Jarek <t0x3e8@gmail.com>
2019-12-04 23:15:04 +0100

    4a does not work yet

---
+++ 04a/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _04a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFrom = 193651;
+            var inputTo = 649729;
+            var counter = 0;
+            for (int i = inputFrom; i < inputTo; i++)
+            {
+                var hasPair = HasNumberPair(i);
+                var isAlwaysIncrease = IsNumberIncreasing(i);
+                if(hasPair && isAlwaysIncrease) 
+                    counter++;
+            }
+
+            Console.WriteLine(counter);
+        }
+        static bool IsNumberIncreasing(int number)
+        {
+            bool result = false;
+            var numberText = number.ToString();
+            for (int i = 1; i < 6; i++)
+            {
+                int prev = (int)Char.GetNumericValue(numberText[i - 1]);
+                int curr = (int)Char.GetNumericValue(numberText[i]);
+                result = curr >= prev;
+                if (!result)
+                    break;
+            }
+
+            return result;
+        }
+        static bool HasNumberPair(int number)
+        {
+            bool result = false;
+            var numberText = number.ToString();
+            for (int i = 0; i < 9; i++)
+            {
+                result = numberText.Contains(string.Concat(i.ToString(), i.ToString()));
+                if (result)
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
