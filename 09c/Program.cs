9e8d8a8805d52718ec293c754dfa8e409188cd6b
Jarosław Jurczyk (JAJU) <jaju@dgs.com>
2019-12-09 18:26:11 +0100

    changes

--- 09c/Program.cs
+++ 09c/Program.cs
@@ -20,6 +20,7 @@ namespace _09c
   // Output: 12077198
   class VM
   {
+    private bool isSettingRead = false;
     private long? initialSignal;
     private long defaultSetting;
     public string ID { get; set; }
@@ -31,159 +32,46 @@ namespace _09c
       this.defaultSetting = defaultSetting;
       this.ID = ID;
     }
-    private long ReadValue(List<long> inputData, int curPos, int parameterPosition)
-    {
-      var opCode = ToArray(inputData[curPos]);
-      long val = 0;
-      var address = inputData[curPos + parameterPosition];
-      int shifted = parameterPosition + 1;
 
-      if (opCode.Length >= shifted && opCode[shifted] == 1)
-        val = inputData[curPos + parameterPosition];
-      else if (opCode.Length >= shifted && opCode[shifted] == 2)
-        val = inputData[(int)referenceBase];
+    private int OperateCurrentPosition(List<long> inputData, int curPos, Func<long, long, long> testOperation)
+    {
+      var val1 = this.ReadValue(inputData, curPos, 1);
+      var val2 = this.ReadValue(inputData, curPos, 2);
+
+      return (int)testOperation(val1, val2);
+    }
+    private List<long> Operate(List<long> inputData, int curPos, Func<long, long, long> mathOperation)
+    {
+      var val1 = this.ReadValue(inputData, curPos, 1);
+      var val2 = this.ReadValue(inputData, curPos, 2);
+      var outputAddress = inputData[curPos + 3];
+
+      if (inputData.Count <= outputAddress)
+        inputData.AddRange(new long[outputAddress - inputData.Count + 1]);
+
+      inputData[(int)outputAddress] = mathOperation(val1, val2);
+
+      return inputData;
+    }
+
+    private long ReadValue(List<long> inputData, int curPos, int opArgumentPosition)
+    {
+      var opCode = ToArray((int)inputData[curPos]);
+      long val = 0;
+      var address = inputData[curPos + opArgumentPosition];
+      int shifted = opArgumentPosition + 1;
+
+      if (opCode.Length > shifted && opCode[shifted] == 1)
+        val = inputData[curPos + opArgumentPosition];
+      else if (opCode.Length > shifted && opCode[shifted] == 2)
+        val = inputData[(int)(referenceBase + inputData[curPos + opArgumentPosition])];
       else
-        val = (inputData.Count < address) ? 0 : inputData[(int)address];
+        val = (inputData.Count < address || address < 0) ? 0 : inputData[(int)address];
 
       return val;
     }
 
-    private static int[] ToArray(long number)
-    {
-      List<int> opCode = new List<int>();
-      while (number > 0)
-      {
-        opCode.Add((int)number % 10);
-        number = number / 10;
-      }
-
-      return opCode.ToArray();
-    }
-  }
-
-  class Program
-  {
-
-
-    static void Main(string[] args)
-    {
-      var inputData = ReadFile("input.txt");
-      int inputParameter = 1;
-
-      RunCode(inputData, inputParameter);
-    }
-
-    private static List<int> RunCode(List<int> inputData, int inputParam)
-    {
-      inputParam = 5;
-      int curPos = 0;
-      bool done = false;
-      while (!done)
-      {
-        if (inputData[curPos] != 0 && (inputData[curPos] % 99) == 0)
-        {
-          done = true;
-          break;
-        }
-
-        switch (inputData[curPos] % 10)
-        {
-          case OP_ADD:
-            inputData = Operate(inputData, curPos, (v1, v2) => { return v1 + v2; });
-            curPos += 4;
-            break;
-          case OP_MUL:
-            inputData = Operate(inputData, curPos, (v1, v2) => { return v1 * v2; });
-            curPos += 4;
-            break;
-          case OP_EQU:
-            inputData = Operate(inputData, curPos, (v1, v2) => { return (v1 == v2) ? 1 : 0; });
-            curPos += 4;
-            break;
-          case OP_JIT:
-            curPos = OperateCurrentPosition(inputData, curPos, (inVal, outVal) =>
-            {
-              return (inVal != 0) ? outVal : curPos += 3;
-            });
-            break;
-          case OP_JIF:
-            curPos = OperateCurrentPosition(inputData, curPos, (inVal, outVal) =>
-            {
-              return (inVal == 0) ? outVal : curPos += 3;
-            });
-            break;
-          case OP_LTH:
-            inputData = Operate(inputData, curPos, (v1, v2) => { return (v1 < v2) ? 1 : 0; });
-            curPos += 4;
-            break;
-          case OP_IN:
-            Console.WriteLine("Provide input number: ");
-            // inputParam = int.Parse(Console.ReadLine());
-            inputData[inputData[curPos + 1]] = inputParam;
-            curPos += 2;
-            break;
-          case OP_OUT:
-            Console.WriteLine("Output: " + inputData[inputData[curPos + 1]]);
-            curPos += 2;
-            break;
-          default:
-            curPos += 1;
-            break;
-        }
-
-        if (curPos >= inputData.Count)
-          done = true;
-      }
-
-      return inputData;
-    }
-
-    static int OperateCurrentPosition(List<int> inputData, int curPos, Func<int, int, int> test)
-    {
-      var opCode = ToArray(inputData[curPos]);
-
-      var val2 = 0;
-      var address2 = inputData[curPos + 2];
-      var val1 = 0;
-      var address1 = inputData[curPos + 1];
-
-      if (opCode.Length >= 3 && opCode[2] == 1)
-        val1 = inputData[curPos + 1];
-      else
-        val1 = inputData[address1];
-      if (opCode.Length >= 4 && opCode[3] == 1)
-        val2 = inputData[curPos + 2];
-      else
-        val2 = inputData[address2];
-
-      return test(val1, val2);
-    }
-
-    static List<int> Operate(List<int> inputData, int curPos, Func<int, int, int> func)
-    {
-      var opCode = ToArray(inputData[curPos]);
-      var val1 = 0;
-      var address1 = inputData[curPos + 1];
-
-      var val2 = 0;
-      var address2 = inputData[curPos + 2];
-      var outputAddress = inputData[curPos + 3];
-
-      if (opCode.Length >= 3 && opCode[2] == 1)
-        val1 = inputData[curPos + 1];
-      else
-        val1 = inputData[address1];
-
-      if (opCode.Length >= 4 && opCode[3] == 1)
-        val2 = inputData[curPos + 2];
-      else
-        val2 = inputData[address2];
-      inputData[outputAddress] = func(val1, val2);
-
-      return inputData;
-    }
-
-    static int[] ToArray(int number)
+    private static int[] ToArray(int number)
     {
       List<int> opCode = new List<int>();
       while (number > 0)
@@ -195,12 +83,139 @@ namespace _09c
       return opCode.ToArray();
     }
 
-    static List<int> ReadFile(string fileName)
+
+    public long RunCode(List<long> inputData)
+    {
+      int curPos = 0;
+      bool done = false;
+      long result = 0;
+
+      Console.WriteLine($"{this.ID} START, curPost: {curPos}");
+
+      while (!done)
+      {
+        if (inputData[curPos] != 0 && (inputData[curPos] % 99) == 0)
+        {
+          done = true;
+          Console.WriteLine($"{this.ID} STOP");
+          break;
+        }
+
+        switch (inputData[curPos] % 10)
+        {
+          case (int)OpCodes.OP_ADD:
+            inputData = Operate(inputData, curPos, (v1, v2) =>
+            {
+              long addResult = v1 + v2;
+              Console.WriteLine($"{this.ID} ADD, {v1} + {v2} = {addResult}");
+              return addResult;
+            });
+            curPos += 4;
+            break;
+          case (int)OpCodes.OP_MUL:
+            inputData = Operate(inputData, curPos, (v1, v2) =>
+            {
+              long mulResult = v1 * v2;
+              Console.WriteLine($"{this.ID} MUL, {v1} * {v2} = {mulResult}");
+              return mulResult;
+            });
+            curPos += 4;
+            break;
+          case (int)OpCodes.OP_EQU:
+            inputData = Operate(inputData, curPos, (v1, v2) =>
+            {
+              int equResult = v1 == v2 ? 1 : 0;
+              Console.WriteLine($"{this.ID} EQU, {v1} == {v2} = {equResult}");
+              return equResult;
+            });
+            curPos += 4;
+            break;
+          case (int)OpCodes.OP_JIT:
+            curPos = OperateCurrentPosition(inputData, curPos, (inVal, outVal) =>
+            {
+              return (inVal != 0) ? outVal : curPos += 3;
+            });
+
+            Console.WriteLine($"{this.ID} JIT {curPos}");
+            break;
+          case (int)OpCodes.OP_JIF:
+            curPos = OperateCurrentPosition(inputData, curPos, (inVal, outVal) =>
+            {
+              return (inVal == 0) ? outVal : curPos += 3;
+            });
+            Console.WriteLine($"{this.ID} JIF {curPos}");
+            break;
+          case (int)OpCodes.OP_LTH:
+            inputData = Operate(inputData, curPos, (v1, v2) =>
+            {
+              int lthResult = (v1 < v2) ? 1 : 0;
+
+              Console.WriteLine($"{this.ID} LTH {lthResult}");
+              return lthResult;
+            });
+            curPos += 4;
+            break;
+          case (int)OpCodes.OP_IN:
+            long parameter = 0;
+
+            if (!this.isSettingRead)
+            {
+              Console.WriteLine($"{this.ID} using settings: {this.defaultSetting}");
+              parameter = this.defaultSetting;
+              this.isSettingRead = true;
+            }
+            else if (this.initialSignal.HasValue)
+            {
+              Console.WriteLine($"{this.ID} using initial signal: {this.initialSignal.Value}");
+              parameter = this.initialSignal.Value;
+              this.initialSignal = null;
+            }
+            else
+            {
+              Console.WriteLine($"{this.ID} using DEFAULT 0");
+              parameter = 0;
+            }
+
+            inputData[(int)inputData[curPos + 1]] = parameter;
+            curPos += 2;
+            break;
+          case (int)OpCodes.OP_OUT:
+            result = ReadValue(inputData, curPos, 1);
+            Console.WriteLine($"{this.ID} OUTPUT {result}");
+            curPos += 2;
+            break;
+          case (int)OpCodes.OP_RBS:
+            this.referenceBase += ReadValue(inputData, curPos, 1);
+            Console.WriteLine($"{this.ID} RBS new referenceBase {referenceBase}");
+            curPos += 2;
+            break;
+        }
+
+        if (curPos >= inputData.Count)
+          done = true;
+      }
+
+      return result;
+    }
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var inputData = ReadFile("input.txt");
+      VM vm = new VM(null, 1, "VM");
+
+      long result = vm.RunCode(inputData);
+      Console.WriteLine(result);
+    }
+
+    static List<long> ReadFile(string fileName)
     {
       var stream = File.OpenRead(fileName);
       var sr = new System.IO.StreamReader(stream);
       var inputText = sr.ReadToEnd();
-      var list = inputText.Split(',').Select(int.Parse).ToList<int>();
+      var list = inputText.Split(',').Select(long.Parse).ToList<long>();
 
       return list;
     }
